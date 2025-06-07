c05b0590646c297b46857d58850ac87ebb8ea88d
fharhanamrin <fharhanamrin028@gmail.com>
2025-06-07 12:51:54 +0700

    feat: initialize project with React, TypeScript, and TailwindCSS
    add basic project structure including Vite config, ESLint setup, and TS configs

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <div className="bg-white shadow">
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores vel cum repudiandae commodi labore facere magnam quisquam accusantium repellat tempora impedit, harum esse, ea iure, laboriosam alias quibusdam veniam et.</p>
+    </div>
+  )
+}
