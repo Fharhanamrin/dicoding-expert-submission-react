67bf5793283af51d12603ae1e8ab1275fd913dbb
fharhanamrin <fharhanamrin028@gmail.com>
2025-06-07 23:48:07 +0700

    feat: initialize project with routing and redux setup
    add basic page components and routing configuration
    set up redux toolkit and required dependencies
    create initial store and api service files

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+
+import ReactDOM from "react-dom/client";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import ChatterDetail from "./pages/ChatterDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Account from "./pages/Account";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: '/detail',
+    element: <ChatterDetail />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/account',
+    element: <Account />,
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/ChatterDetail/index.tsx
@@ -0,0 +1,5 @@
+export default function ChatterDetail() {
+  return (
+      <div>ChatterDetail</div>
+  )
+}
